86f84ae8da8507ccc6be39201e1d09035d393ff7
Tom Tarpey <tomtarpeydev@gmail.com>
2024-05-29 23:33:04 +0100

    finished-rag

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from constants import *
 
-# the prompt: we will be changing this soon
-prompt = "hello world!"
-
-# Note: we must use the same embedding model that we used when uploading the docs
 
 
-# Querying the vector database for "relevant" docs then create a retriever
 
-# create a context by using the retriever and getting the relevant docs based on the prompt
-
-# show the thought process by looping over all relevant docs, showing the source and the content
-
-
-# build a prompt template using the query and the context and build the prompt with context
+embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+document_vectorstore = PineconeVectorStore(index_name=PINECONE_INDEX, embedding=embeddings)
+retreiver = document_vectorstore.as_retriever()
+llm = ChatOpenAI(temperature=0.7)
+template = PromptTemplate(template="{query} Context: {context}", input_variables=["query", "context"])
 
 
-# Asking the LLM for a response from our prompt with the provided context using CatOpenAI and invoking it
-# Then print the results content
+prompt = ""
+
+while(prompt != "exit"):
+    prompt = input("PROMPT (type 'exit' to close the app)> ")
+    if prompt == "exit":
+        print("Thank you for using the Rag App!\n\n")
+        break
+    context = retreiver.get_relevant_documents(prompt)
+    prompt_with_context = template.invoke({"query": prompt, "context": context})
+    results = llm.invoke(prompt_with_context)
+    print(results.content)
